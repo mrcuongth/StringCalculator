0583e3b5eeab894777e42246ac048bc394cabc3c
Trinh Huy Cuong <mr.cuongth@gmail.com>
2013-06-05 14:33:06 +0700

    [StringCalc0506] new test with string contains more than 1 numbers

--- TDDSampleWiki/TDDSampleWikiTests/TestStringCalculator0506.m
+++ TDDSampleWiki/TDDSampleWikiTests/TestStringCalculator0506.m
@@ -20,6 +20,11 @@ describe(@"Test string calculator 05/06", ^{
     it(@"Test with string contains 1 number", ^{
         [[theValue([StringCalc0506 calc:@"10"]) should] equal:theValue(10)];
     });
+    
+    it(@"Test with string contains 2 or more numbers", ^{
+        [[theValue([StringCalc0506 calc:@"1,2"]) should] equal:theValue(3)];
+        [[theValue([StringCalc0506 calc:@"3,4,5"]) should] equal:theValue(12)];
+    });
 });
 
 SPEC_END
