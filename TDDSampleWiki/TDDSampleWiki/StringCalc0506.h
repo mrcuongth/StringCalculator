3e853ea571ec1e95e91e2cb24f7f232ccae861a8
Trinh Huy Cuong <mr.cuongth@gmail.com>
2013-06-05 14:26:44 +0700

    [StringCalc0506] Add class StringCalc with function Calc

---
+++ TDDSampleWiki/TDDSampleWiki/StringCalc0506.h
@@ -0,0 +1,15 @@
+//
+//  StringCalc0506.h
+//  TDDSampleWiki
+//
+//  Created by Trinh Huy Cuong  on 6/5/13.
+//  Copyright (c) 2013 QSoftVietNam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StringCalc0506 : NSObject
+
++ (NSInteger) calc:(NSString*)str;
+
+@end
---
+++ TDDSampleWiki/TDDSampleWiki/StringCalc0506.m
@@ -0,0 +1,17 @@
+//
+//  StringCalc0506.m
+//  TDDSampleWiki
+//
+//  Created by Trinh Huy Cuong  on 6/5/13.
+//  Copyright (c) 2013 QSoftVietNam. All rights reserved.
+//
+
+#import "StringCalc0506.h"
+
+@implementation StringCalc0506
+
++ (NSInteger) calc:(NSString*)str{
+    return 1;
+}
+
+@end
