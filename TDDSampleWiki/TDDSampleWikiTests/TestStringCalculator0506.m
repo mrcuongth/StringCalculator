635955d6c8b9ee287c1574dabf896283fcad34c6
Trinh Huy Cuong <mr.cuongth@gmail.com>
2013-06-05 14:25:21 +0700

    [StringCalc0506] First test case with empty or nil string

---
+++ TDDSampleWiki/TDDSampleWikiTests/TestStringCalculator0506.m
@@ -0,0 +1,22 @@
+//
+//  TestStringCalculator0506.m
+//  TDDSampleWiki
+//
+//  Created by Trinh Huy Cuong  on 6/5/13.
+//  Copyright (c) 2013 QSoftVietNam. All rights reserved.
+//
+
+#import "Kiwi.h"
+
+SPEC_BEGIN(TestStringCalc0506)
+
+describe(@"Test string calculator 05/06", ^{
+    it(@"First test with empty or nil string", ^{
+        [[theValue([StringCalc0506 calc:@""]) should] equal:theValue(0)];
+        [[theValue([StringCalc0506 calc:nil]) should] equal:theValue(0)];
+    });
+});
+
+SPEC_END
+
+
