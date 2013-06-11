b35daa5512dddd553f8acf04b177b344e00c3165
Trinh Huy Cuong <mr.cuongth@gmail.com>
2013-06-11 14:27:09 +0700

    Create classes

---
+++ TDDSampleWiki/TDDSampleWiki/StringCalc1106.h
@@ -0,0 +1,15 @@
+//
+//  StringCalc1106.h
+//  TDDSampleWiki
+//
+//  Created by Trinh Huy Cuong  on 6/11/13.
+//  Copyright (c) 2013 QSoftVietNam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StringCalc1106 : NSObject
+
++ (NSInteger) calc:(NSString*)str;
+
+@end
---
+++ TDDSampleWiki/TDDSampleWiki/StringCalc1106.m
@@ -0,0 +1,17 @@
+//
+//  StringCalc1106.m
+//  TDDSampleWiki
+//
+//  Created by Trinh Huy Cuong  on 6/11/13.
+//  Copyright (c) 2013 QSoftVietNam. All rights reserved.
+//
+
+#import "StringCalc1106.h"
+
+@implementation StringCalc1106
+
++ (NSInteger) calc:(NSString*)str{
+    return 10;
+}
+
+@end
