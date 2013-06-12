89f7a5b631fca5e0cde3d67c4b90fed578c7f500
Trinh Huy Cuong <mr.cuongth@gmail.com>
2013-06-12 14:13:28 +0700

    Refactor

--- TDDSampleWiki/TDDSampleWiki/StringCalc1206.m
+++ TDDSampleWiki/TDDSampleWiki/StringCalc1206.m
@@ -15,9 +15,7 @@
         return 0;
     }
     
-    return 11;
-    
-    NSArray *arr = [str componentsSeparatedByString:@","];
+    NSArray *arr = [str componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"\n,"]];
     NSInteger result = 0;
     
     for (NSString *s in arr) {
