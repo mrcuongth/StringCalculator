ae9087b41c8908e086df9dc246c12f3af822845c
Trinh Huy Cuong <mr.cuongth@gmail.com>
2013-06-13 13:54:45 +0700

    RED

--- TDDSampleWiki/TDDSampleWikiTests/TestStringCalc1306.m
+++ TDDSampleWiki/TDDSampleWikiTests/TestStringCalc1306.m
@@ -16,6 +16,10 @@ describe(@"Test string calculator 13.06", ^{
         [[theValue([StringCalc1306 calc:@""]) should] equal:theValue(0)];
         [[theValue([StringCalc1306 calc:nil]) should] equal:theValue(0)];    
     });
+    
+    it(@"test with string contains 1 number", ^{
+        [[theValue([StringCalc1306 calc:@"5"]) should] equal:theValue(5)];
+    });
 });
 
 SPEC_END
